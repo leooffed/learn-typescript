28fd668fd5be94f27f13993247f3513a7ad258a6
leo libres <developpeurclear@gmail.com>
2025-08-18 00:23:03 +0100

    Learn the book Js

--- learn.js
+++ learn.js
@@ -40,4 +40,80 @@ console.log("&&") // La logique {ET}
 console.log("||") // La Logique (OU)
 console.log() // Les Ternaire
 
+let myName = "leo"
+
+if (myName && myName.toLowerCase()) {
+    console.log(myName)
+} else {
+    console.log("is not to Upper")
+}
+
+// Litteraux d'objet
+let empty_object = {}
+
+let stoo = {
+    "fist-name": "Jerome",
+    "last-name": "Joe",
+    "nickname": "Niga"
+}
+
+let flight = {
+    airline: "Oceanic",
+    number: 815,
+    departure: {
+        IATA: "SYD",
+        time: "2003-12-22 14:55",
+        city: "London"
+    },
+    arrival: {
+        IATA: "LAX",
+        time: "2004-09-11 11:03",
+        city: "Los Angeles"
+    },
+    equipement: {
+        model: "Jorimu 838"
+    },
+    status: "Kuse"
+}
+
+// Recuperation
+console.log(stoo['fist-name'])
+console.log(flight.departure.IATA)
+
+console.log(stoo["login"])
+
+// using || or
+let middle = stoo["login"] || "(none)"
+let status = flight.status || "unknown"
+console.log(middle)
+console.log(status)
+
+// utilisation && pour se premunir
+flight.equipement
+flight.equipement.model
+console.log(flight.equipement && flight.equipement.model)
+
+
+// update
+stoo['fist-name'] = "Jerome"
+stoo['middle-name'] = 'Lester'
+stoo["last-name"] = "Curly"
+flight.equipement = {
+    model: "oeiry 777"
+}
+
+flight.status = 'overdue'
+
+// Reference
+
+let x = stoo
+x.nickname = 'Curlo'
+let nick = stoo.nickname
+console.log(nick)
+
+let a = {}, b = {}, c = {}
+
+console.log(a = b = c = {})
+
+// Prototype
 
